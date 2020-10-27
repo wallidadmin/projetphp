35e51e4ea6da4e05ce8f9e0aaddb1fa0402fff34
wallidadmin <w.bensmina@codeur.online>
2020-10-27 09:46:59 +0100

    Voici mon calcul de surface

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+$longeur = $_POST['longueur'];
+$largeur = $_POST['largeur'];
+$surface = $longeur*$largeur;
+
+var_dump ($longeur);
+echo "$longeur";
+
+
+?>
+
+<form action="" method="post";>
+
+Longeur: <input type="text" name="longueur"><br><br>
+Largeur: <input type="text" name="largeur"><br><br>
+<input type="submit" value="calculer !"></input>
+
+</form>
+
+<?php 
+
+echo "Surface = $surface";
+
+?>
